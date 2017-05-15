6f347bdd74ab0e296d9fb4bf197ad4f753aa1bc5
Michael Kuehl <michael@ratchet.cc>
2017-05-15 22:31:21 +0200

    changed the OG meta-tags and urls

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,14 +1,16 @@
 
 module MetaTagsHelper
 
-	def og_html(url, type, title, image, description)
+	def og_html(url, type, title, description, image)
 		og = <<-EOF
 			<meta property='fb:app_id' content='#{ENV['fb_app_id']}' />
 			<meta property='og:url' content='#{url}' />
 			<meta property='og:type' content='#{type}' />
 			<meta property='og:title' content='#{title}' />
 			<meta property='og:description' content='#{description}' />
-			<meta property='og:image' content='#{image}' />
+			<meta property='og:image' content='#{image['url']}' />
+			<meta property='og:image:width' content='#{image['width']}' />
+			<meta property='og:image:height' content='#{image['height']}' />
 		EOF
 		og.html_safe
 	end
@@ -16,7 +18,7 @@ module MetaTagsHelper
 	def fb_meta_tags(item)
 		#affiliate_url = "https://www.amazon.de/dp/#{item[:id]}/?tag=#{ENV['amzn_partner_id']}"
 		url = "#{ENV['base_url']}/a/#{item[:details]['region']}/#{item[:id]}"
-		og_html url, 'book', item[:title], item[:images][0]['url'], item[:summary]
+		og_html url, 'book', item[:title], item[:summary], item[:images][1]
 	end
 
 end
