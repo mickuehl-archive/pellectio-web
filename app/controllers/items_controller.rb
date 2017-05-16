3ac3a03b2aca3f8fa44a2a28cfe5c48d74d04717
Michael Kuehl <michael@ratchet.cc>
2017-05-16 08:13:37 +0200

    fixed NPE

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,20 +6,23 @@ class ItemsController < ApplicationController
 		@asin = params[:asin]
 
 		@results = ItemService.new.lookup @asin, @region
-		@related = @results[0][:details]['related_products']
-		@features = @results[0][:details]['feature']
-		#puts "#{@results}"
+
+		if @results.empty?
+			redirect_to root_path
+		else
+			@related = @results[0][:details]['related_products']
+			@features = @results[0][:details]['feature']
+		end
+
 	end
 
 	def affiliate
-		#puts "#{request.env['HTTP_HOST']}"
-		#puts "#{request.env['HTTP_USER_AGENT']}"
 		user_agent = request.env['HTTP_USER_AGENT']
 		region = params[:region]
 		asin = params[:asin]
 
 		if user_agent.start_with? 'facebook'
-			#affiliate_url = "#{ENV['base_url']}/i/#{region}/#{asin}"
+			# do not redirect, its FB scraping !
 			@results = ItemService.new.lookup asin, region
 			@related = @results[0][:details]['related_products']
 			@features = @results[0][:details]['feature']
