3073b36c99b44c5f244c86f9ce7ff69c95c02ddd
Michael Kuehl <michael@ratchet.cc>
2017-05-19 09:07:04 +0200

    minor sytle changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  force_ssl if: :ssl_configured?
+
+private
+
+  def ssl_configured?
+    Rails.env.production?
+  end
+
 end
