a2331b2d1f853d0bb4ed54210d130679ca0fd980
Michael Kuehl <michael@ratchet.cc>
2017-08-04 09:43:36 +0200

    added Googlebot to the list of clients that are not redirected to the affiliate link

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
 		user_agent = request.env['HTTP_USER_AGENT']
 		remote_addr = request.env['REMOTE_ADDR']
 		
-		puts "user_agent=#{user_agent}, remote_addr=#{remote_addr}"
+		#puts "user_agent=#{user_agent}, remote_addr=#{remote_addr}"
 
 		region = params[:region]
 		asin = params[:asin]
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
 		@results = ItemService.new.lookup asin, region
 
 		if is_web_crawler? user_agent
-			# do not redirect, its FB, LinkedIn etc scraping !
+			# do not redirect, its FB, LinkedIn, Google etc scraping !
 			@related = @results['related']
 			render :show
 		else
@@ -41,11 +41,13 @@ class ItemsController < ApplicationController
 
 private
 
-	#  LinkedInBot/1.0
+	# LinkedInBot/1.0
 	# facebookexternalhit/1.1
+	# user_agent=Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
 	def is_web_crawler?(user_agent)
 		return true if user_agent.start_with? 'facebook'
 		return true if user_agent.start_with? 'LinkedInBot'
+		return true if user_agent.include? 'Googlebot'
 		false
 	end
 
